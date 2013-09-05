bf51df46db38e9c1fac3f05c17e40ad64729e5b0
Max F. Albrecht <1@178.is>
2013-09-05 18:30:41 +0300

    [remake] dev/diff.sh: reformat old style with `recess` before `diff`ing

--- dev/diff.sh
+++ dev/diff.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 # compare refactored and old stylesheet
+########################################
 
-grunt && \
-git diff -w --patience --no-index --color-words \
-  luci/themes/bootstrap/htdocs/luci-static/bootstrap/cascade.css \
-  dist/css/bootstrap.css
+BUILD="grunt"
+SORT="./node_modules/grunt-recess/node_modules/recess/bin/recess --compile"
+DIFF="git diff -w --patience --no-index --color-words"
+
+OLD="luci/themes/bootstrap/htdocs/luci-static/bootstrap/cascade.css"
+NEW="dist/css/bootstrap.css"
+
+###
+
+$BUILD && \
+$SORT "$OLD" > "$OLD"_sorted && \
+$DIFF "$OLD"_sorted "$NEW"
